d9bb25166a5350b448b61755758ebaf1b0803a9f
nasimpc <nasimpcm@gmail.com>
2024-01-14 21:06:18 +0530

    TS_end

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const todo_1 = __importDefault(require("./routes/todo"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-app.listen(3000);
+app.use(body_parser_1.default.json());
+app.use(todo_1.default);
+app.listen(4000);
--- dist/routes/todo.js
+++ dist/routes/todo.js
@@ -1,37 +1,34 @@
-import { Router } from 'express'
-import { Todo } from '../models/todo';
-
-let todos: Todo[] = [];
-
-const router = Router();
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
 router.get('/', (req, res, next) => {
-    res.status(200).json({ todos: todos })
+    res.status(200).json({ todos: todos });
 });
-
 router.post('/todo', (req, res, next) => {
-    const newTodo: Todo = {
+    const body = req.body;
+    const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ success: true, todo: newTodo, todos: todos });
 });
-
 router.put('todo/:todoId', (req, res, next) => {
-    console.log(req.params)
-    const tid = req.params.todoId;
+    const params = req.params;
+    const body = req.body;
+    const tid = params.todoId;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: 'Updated Todo', todos: todos });
     }
     res.status(404).json({ message: 'Could not find todo for this id' });
 });
-
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
     res.status(200).json({ message: 'Deleted todo', todos: todos });
 });
-
-export default router;
+exports.default = router;
